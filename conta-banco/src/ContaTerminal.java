0f23d21100844f75d7065056ae19f8f6c3b9587d
Welton <wel.indumill@gmail.com>
2024-04-30 19:32:03 -0300

    att

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int numero ;
+        String agencia ;
+        String nomeCliente ;
+        double saldo;
+
+        System.out.println("Digite o seu nome?");
+        nomeCliente = input.next();
+        System.out.println("Digite Sua agencia?");
+        agencia = input.next();
+        System.out.println("Digite seu numero?");
+        numero = input.nextInt();
+        System.out.println("Saldo que possui em conta?");
+        saldo = input.nextDouble();
+        
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque.");
     }
 }
